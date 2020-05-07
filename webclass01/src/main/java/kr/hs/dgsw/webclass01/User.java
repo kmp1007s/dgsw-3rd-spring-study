d6fbb9aa1e79b887d7d79d1de76fb9a74cf2e907
SaeHan Kim <kmp1007s@naver.com>
2020-05-07 15:10:21 +0900

    Lombok ToString
    Lombok ToString

--- webclass01/src/main/java/kr/hs/dgsw/webclass01/User.java
+++ webclass01/src/main/java/kr/hs/dgsw/webclass01/User.java
@@ -4,18 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class User {
   private String id;
   private String name;
   private String email;
-
-  @Override
-  public String toString() {
-    return id + "/" + name + "/" + email;
-  }
 }
