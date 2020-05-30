71126430d8799fff94fa09f9f5e7650394cae3ce
SaeHan Kim <kmp1007s@naver.com>
2020-05-30 14:36:37 +0900

    Add ignore rules
    Add ignore rules

--- webclass02/src/main/java/kr/hs/dgsw/webclass02/Controller/AttachmentController.java
+++ webclass02/src/main/java/kr/hs/dgsw/webclass02/Controller/AttachmentController.java
@@ -38,7 +38,7 @@ public class AttachmentController {
 
   @PostMapping("/attachment")
   public AttachmentProtocol upload(@RequestPart MultipartFile srcFile) {
-    String destFileName = "E:/saehan/3rd-web/dgsw_3rd_spring_study/webclass02/src/main/resources/static/"
+    String destFileName = "E:/saehan/3rd-web/dgsw_3rd_spring_study/webclass02/src/main/resources/static/image/"
         + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd/")) + UUID.randomUUID().toString() + "_"
         + srcFile.getOriginalFilename();
 
